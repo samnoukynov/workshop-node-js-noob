0425bd49524dc163f20c21e9c53ebdd009be4484
Sam Nouk <snoukeu@gmail.com>
2017-11-21 16:52:06 +0100

    exercice3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,15 @@
-exports.run = function(input) {
-  console.log("Exercise #3");
+ exports.run = function(input) {
+  
+ 
+
+  var array = input;
+  var resultat = 1;
+  for ( var i=0 ; i < array.length ; i++)
+  {
+  	if ( array[i].multiply == true )
+  	{
+  		resultat = resultat * array[i].value ;
+  	}
+  }
+  console.log(resultat);
 };
