824408cbbc081b408179ab3316c77fa6cee2e9c7
Sam Nouk <snoukeu@gmail.com>
2017-11-21 17:15:50 +0100

    exercice4

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,8 +2,11 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
-
+  var retour = () =>
+  {
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
   /** */
+  };
+  
 };
